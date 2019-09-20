511c575deeb23026388845220a2d8bbce6ccb632
Song Li <songl@outlook.com>
2019-09-20 12:00:26 +0800

    Get available voices

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/foundation.dart'
     show debugDefaultTargetPlatformOverride;
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'http.dart';
 
 void main() {
   // See https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override
@@ -10,15 +12,16 @@ void main() {
 }
 
 const regions = const [
-  const Region('West US', 'https://westus.tts.speech.microsoft.com/cognitiveservices/voices/list'),
-  const Region('Southeast Asia', 'https://southeastasia.tts.speech.microsoft.com/cognitiveservices/voices/list'),
+  const Region('Southeast Asia', 'https://southeastasia.tts.speech.microsoft.com/cognitiveservices/voices/list', 'https://southeastasia.api.cognitive.microsoft.com/sts/v1.0/issueToken'),
+  const Region('West US', 'https://westus.tts.speech.microsoft.com/cognitiveservices/voices/list', 'https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken'),
 ];
 
 class Region {
   final String regionName;
   final String voiceListUrl;
+  final String authUrl;
 
-  const Region(this.regionName, this.voiceListUrl);
+  const Region(this.regionName, this.voiceListUrl, this.authUrl);
 }
 
 class MyApp extends StatelessWidget {
@@ -44,7 +47,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Region selectedRegion = regions.first;
+  Region _selectedRegion = regions.first;
+  String _text = "";
 
   @override
   void initState() {
@@ -67,31 +71,29 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Padding(
         padding: EdgeInsets.all(8.0),
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget> [
-              Text(
-                'Selected region:',
-              ),
-              Text(
-                '${selectedRegion.regionName}\n',
-              ),
-              Text(
-                'URL for list of available voices:',
-              ),
-              Text(
-                '${selectedRegion.voiceListUrl}\n',
-              ),
-              Text(
-                'Available voices:',
-              ),
-              Text(
-                'TODO\n',
-              ),
-            ],
-          ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget> [
+            Text(
+              'Selected region:',
+            ),
+            Text(
+              '${_selectedRegion.regionName}\n',
+            ),
+            Text(
+              'URL for list of available voices:',
+            ),
+            Text(
+              '${_selectedRegion.voiceListUrl}\n',
+            ),
+            Text(
+              'Available voices:',
+            ),
+            Expanded(
+              child: Text(_text),
+            ),
+          ],
         ),
       ),
     );
@@ -100,14 +102,32 @@ class _MyHomePageState extends State<MyHomePage> {
   List<CheckedPopupMenuItem<Region>> get _buildRegionsWidgets => regions
     .map((r) => CheckedPopupMenuItem<Region>(
       value: r,
-      checked: selectedRegion == r,
+      checked: _selectedRegion == r,
       child: Text(r.regionName),
     ))
     .toList();
 
   void _selectRegionHandler(Region region) {
     setState(() {
-      selectedRegion = region;
+      _selectedRegion = region;
+      String token = '';
+      dio.post<String>(_selectedRegion.authUrl,
+        options: Options(
+          headers: {
+            'Ocp-Apim-Subscription-Key':'my key (actually this should be different per region)',
+          },
+          responseType: ResponseType.plain
+        )).then((r) {
+          token = r.data;
+          dio.get<String>(_selectedRegion.voiceListUrl,
+            options: Options(
+              headers: {
+                'Authorization':'Bearer '+token,
+              }
+            )).then((r) {
+              _text = r.data;
+          }).catchError(print);
+        }).catchError(print);
     });
   }
 }
